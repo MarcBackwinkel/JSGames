ad45e2b98fbbbae7541538fdcd8438f29fd5465d
MarcBackwinkel <marc.backwinkel@gmail.com>
2021-03-10 16:56:35 +0000

    Guess The Number is in

---
+++ 01_guessTheNumber/src/main.js
@@ -0,0 +1,16 @@
+const myGuess = Math.floor(Math.random() * 20) + 1;
+let guesses = 0;
+let guess;
+
+while (guess !== myGuess){
+    guess = parseInt(prompt("What number am I thinking of?"));
+    guesses++;
+
+    if (guess < myGuess){
+        alert("Higher.")
+    } else if (guess > myGuess) {
+        alert("Lower.");
+    }
+}
+
+alert(`Well done! You got it in ${guesses} guesses!`);
